04963c063c32cd1b97ac0842e032c0a435907b19
dance2die <dontbothermewithspam@gmail.com>
2016-05-29 22:20:11 -0400

    refactor

--- Demo.RandomProjects.Anagram/Program.cs
+++ Demo.RandomProjects.Anagram/Program.cs
@@ -23,25 +23,25 @@ namespace Demo.RandomProjects.Anagram
 		}
 
 		// http://stackoverflow.com/a/756083/4035
-		private static IEnumerable<char[]> GetStringPermutations(char[] characters, int start, int length)
+		private static IEnumerable<char[]> GetStringPermutations(char[] characters, int recursionDepth, int maxDepth)
 		{
-			if (start == length)
+			if (recursionDepth == maxDepth)
 			{
 				yield return characters;
 			}
 			else
 			{
-				for (int i = start; i <= length; i++)
+				for (int i = recursionDepth; i <= maxDepth; i++)
 				{
-					Swap(ref characters[start], ref characters[i]);
+					Swap(ref characters[recursionDepth], ref characters[i]);
 
 					char[] charactersCopy = new string(characters).ToCharArray();
-					foreach (var stringPermutation in GetStringPermutations(charactersCopy, start + 1, length))
+					foreach (var stringPermutation in GetStringPermutations(charactersCopy, recursionDepth + 1, maxDepth))
 					{
 						yield return stringPermutation;
 					}
 
-					Swap(ref characters[start], ref characters[i]);
+					Swap(ref characters[recursionDepth], ref characters[i]);
 				}
 			}
 		}

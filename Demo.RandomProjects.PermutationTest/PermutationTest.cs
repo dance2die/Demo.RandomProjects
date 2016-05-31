da72a23f836c270e7b76195a57ce7cdac1967606
dance2die <dontbothermewithspam@gmail.com>
2016-05-31 14:41:57 -0400

    Integer permutation test passes.

--- Demo.RandomProjects.PermutationTest/PermutationTest.cs
+++ Demo.RandomProjects.PermutationTest/PermutationTest.cs
@@ -3,16 +3,98 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Demo.RandomProjects.PermutationTest
 {
 	public class PermutationTest : BaseTest
 	{
+		private readonly Permutation _sut = new Permutation();
+
 		public PermutationTest(ITestOutputHelper output) : base(output)
 		{
 		}
 
+		[Fact]
+		public void TestIntegerPumutation()
+		{
+			var expected = new List<IEnumerable<int>>
+			{
+				new [] {1, 2, 3},
+				new [] {1, 3, 2},
+				new [] {2, 1, 3},
+				new [] {2, 3, 1},
+				new [] {3, 1, 2},
+				new [] {3, 2, 1}
+			};
 
+			var actual = _sut.GetPermutations(3).ToList();
+
+			Assert.True(IsMultidimensionalArraySequenceEqual(expected, actual));
+		}
+
+		/// <summary>
+		/// Compare a List of integer arrays.
+		/// </summary>
+		//private bool IsMultidimensionalArraySequenceEqual(List<int[]> list1, List<int[]> list2)
+		private bool IsMultidimensionalArraySequenceEqual(List<IEnumerable<int>> list1, List<IEnumerable<int>> list2)
+		{
+			for (int i = 0; i < list1.Count; i++)
+			{
+				if (!list1[i].SequenceEqual(list2[i]))
+					return false;
+			}
+
+			return true;
+		}
+
+		//[Fact]
+		//public void TestStringPumutation()
+		//{
+
+		//}
+
+	}
+
+	public class Permutation
+	{
+		public List<IEnumerable<int>> GetPermutations(int permutationCount)
+		{
+			var list = Enumerable.Range(1, permutationCount).ToList();
+
+			return GetPermutations(list, 0, list.Count - 1).ToList();
+		}
+
+		private IEnumerable<IEnumerable<int>> GetPermutations(
+			IEnumerable<int> enumerable, int startCount, int permutationCount)
+		{
+			var list = enumerable.ToList();
+
+			if (startCount == permutationCount)
+				yield return list;
+
+			for (int i = startCount; i <= permutationCount; i++)
+			{
+				Swap(list, startCount, i);
+
+				foreach (var permutation in GetPermutations(list.ToArray(), startCount + 1, permutationCount))
+					yield return permutation;
+			}
+		}
+
+		private void Swap(IList<int> list, int listIndex1, int listIndex2)
+		{
+			try
+			{
+				var temp = list[listIndex1];
+				list[listIndex1] = list[listIndex2];
+				list[listIndex2] = temp;
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+			}
+		}
 	}
 }
